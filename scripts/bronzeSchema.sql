15a808be19d5a417e9415df76aef430b5f307d3c
Ssemwogerere Abraham <ssem.abraham96@gmail.com>
2025-11-09 12:13:58 +0300

    Create the silver and gold schemas and change drop the members_raw table and recreate it as ugacooq_raw table with a check.

--- scripts/bronzeSchema.sql
+++ scripts/bronzeSchema.sql
@@ -1,8 +1,37 @@
+/* 
+=================================================================
+Create Database Schemas and the bronze
+=================================================================
+Script purpose:
+    This script creates the new schemas (Bronze,Silver & Gold) in the already created database named 'UGACOOQDB'.
+    Also creates a table in the bronze layer called 'ugacooq_raw' after checking if the table exists.
+    If the table exists in, its dropped and recreated.
+
+Warning:
+    Running this script will delete the ugacooq_raw table. Please proceed with caustion or make sure you have abackup
+    before runing the script.
+
+*/
+
+---switch to the UGACOOQ Database
+USE UGACOOQDB;
+
+---Create schemas
 CREATE SCHEMA bronze;
 GO
 
+CREATE SCHEMA silver;
+GO
 
-CREATE TABLE bronze.members_raw (
+CREATE SCHEMA gold;
+GO
+
+---Drop and recreate the ugacooq_raw table
+IF OBJECT_ID('bronze.ugacooq_raw') IS NOT NULL
+    DROP TABLE bronze.ugacooq_raw;
+GO
+
+CREATE TABLE bronze.ugacooq_raw (
     id INT IDENTITY(1,1) PRIMARY KEY,
     ingested_at DATETIME2 NOT NULL,
     source_sheet NVARCHAR(255),
@@ -11,6 +40,4 @@ CREATE TABLE bronze.members_raw (
     row_hash CHAR(64) NOT NULL,
     ingest_batch_id UNIQUEIDENTIFIER NOT NULL
 );
-GO
-
 
